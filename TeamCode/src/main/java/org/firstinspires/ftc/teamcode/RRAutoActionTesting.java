6ced44e3ae582a3058b35b5d100fed3ce911e795
Nathan Morris <nmorris@alberton.k12.mt.us>
2025-01-04 23:25:33 -0700

    RRAutoActionTesting follows the tutorial video from FTC 6282 Simi Valley Robotics: https://youtu.be/uBwVSRxvpB8?si=0o4t_w6YdL_58o8R It is an example of how to add actions for other mechanisms (a servo here) to complete during a roadrunner autonomous routine.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRAutoActionTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRAutoActionTesting.java
@@ -1,12 +1,19 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.acmerobotics.roadrunner.Actions;
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ftc.Actions;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+
+// This program follows the tutorial video from FTC 6282 Simi Valley Robotics: https://youtu.be/uBwVSRxvpB8?si=0o4t_w6YdL_58o8R
+// It is an example of how to add actions for other mechanisms (a servo here) to complete during a roadrunner autonomous routine.
 @Autonomous(name="RR Auto Action Testing")
 public class RRAutoActionTesting extends LinearOpMode {
     @Override
@@ -16,14 +23,17 @@ public class RRAutoActionTesting extends LinearOpMode {
 
         waitForStart();
 
+        // runBlocking runs actions in blocking sequence so each action waits until the previous one is done
         Actions.runBlocking(
                 drive.actionBuilder(new Pose2d(0,0,0))
                 .lineToX(64)
-                .stopAndAdd(new PatientServoAction(intakeLeft, 0)))
+                .stopAndAdd(new PatientServoAction(intakeLeft, 1)) //
                 .lineToX(0)
+                .stopAndAdd(new ServoAction(intakeLeft,0))
                 .build());
     }
 
+    // a basic action like this returns false immediately so the runBlocking moves on to the next action while this completes
     public class ServoAction implements Action {
         Servo servo;
         double position;
@@ -36,14 +46,19 @@ public class RRAutoActionTesting extends LinearOpMode {
         @Override
         public boolean run(@NonNull TelemetryPacket telemetryPacket) {
             servo.setPosition(position);
-            return false;
+            return false; // returning true causes the action to run again, returning false causes it to cease
         }
     }
 
+    // This doesn't immediately return false - it keeps running until the return statement is false
+    // (i.e. it's been running for 3 seconds or motor has gotten to position).
+    // Thus the runBlocking action doesn't move on until this one is done
     public class PatientServoAction implements Action {
         Servo servo;
         double position;
         ElapsedTime timer;
+
+        boolean hasInitialized;
 
         public PatientServoAction(Servo s, double p) {
             this.servo = s;
@@ -52,13 +67,14 @@ public class RRAutoActionTesting extends LinearOpMode {
 
         @Override
         public boolean run(@NonNull TelemetryPacket telemetryPacket) {
-            if (timer == null){
+            if (!hasInitialized){
                 timer = new ElapsedTime();
                 servo.setPosition(position);
             }
 
             // do we need to keep running?
             return timer.seconds() < 3;
+            // or return motor.getPosition == targetPosition;
         }
     }
 }
